1479e2f599f4311be722a1a9cedd07839fba9b52
raebits <raebitsdevs@gmail.com>
2022-09-30 14:55:29 +0700

    add homebrew auto completions (not working yet)

--- config.fish
+++ config.fish
@@ -1,24 +1,16 @@
-# set USE_POWERLINE "true" # use powerline
-
-# starship init fish | source
-
-# if status is-interactive
-#   set FLINE_PATH $HOME/.config/fish/fishline
-#   source $FLINE_PATH/init.fish
-# end
-
 if status is-interactive
   # Commands to run in interactive sessions can go here
 end
 
-### exa ###
+### EXA ###
 if type -q exa
   alias ll "exa -l -g --icons" # exa aliasing
   alias llt "ll --tree" # exa aliasing tree
   alias lla "ll -a" # exa aliasing
 end
-### /exa ###
+### End Of EXA ###
 
+alias "g"="git" # git alias
 alias "ex"="exit" # exit alias
 alias "v"="nvim" # Neovim alias
 alias "vi"="nvim" # Neovim alias
@@ -27,18 +19,30 @@ alias "vim"="nvim" # Neovim alias
 alias "clr"="clear" # clear alias
 alias "c"="clear" # clear alias
 
-### nvm ###
+### NVM ###
 # nvm config uses the oh-my-fish nvm plugin
 
-### homebrew ###
+### BREW ###
 export HOMEBREW_DIR="$HOME/.linuxbrew" # set homebrew directory
 eval (/home/madness/.linuxbrew/bin/brew shellenv) # fetch env for homebrew through homebrew prebuild bin
+
+# autocompletion #
+if test -d (brew --prefix)"/share/fish/completions"
+  set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/completions
+end
+
+if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+  set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+end
+# End Of autocompletion #
+
+### End Of BREW ###
 
 ### yarn ###
 # Somehow yarn config already pulled from zsh env
 
 ### variables ###
-# Somehome env path already pulled from zsh env
+# Some env path already pulled from zsh env
 
-set PATH $HOME/Applications/VSCode-linux-x64/bin $PATH
-set PATH $HOME/.go/bin $PATH
+set PATH $HOME/Applications/VSCode-linux-x64/bin $PATH # add VSCode bin path
+set PATH $HOME/.go/bin $PATH # add golang bin path
